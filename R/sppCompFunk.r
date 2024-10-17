cba1879341a4661ec9f18230b6393619dd613fef
chisquareotops <nick.grunloh@noaa.gov>
2024-10-16 18:06:23 -0700

    trying askpass for getSppPacfinData only and adding the missing manual entries

--- R/sppCompFunk.r
+++ R/sppCompFunk.r
@@ -221,8 +221,8 @@ getPacfinSppData = function(year, save=F, fromFile=F){
 				#PacFIN connection
 				writeLines("\nReading PacFIN Species Data From PacFIN Connection...")
 				#template connection string:"jdbc:oracle:thin:@//database.hostname.com:port/service_name_or_sid"
-				name =  getPass::getPass('PacFIN User: ')
-				password  = getPass::getPass('Password: ')
+				name = askpass::askpass("PacFIN User: ") #getPass::getPass('PacFIN User: ')
+				password = askpass::askpass("Password: ") #getPass::getPass('Password: ')
 				oCon = RJDBC::dbConnect(oDrv, 'jdbc:oracle:thin:@//pacfindb.psmfc.org:2045/pacfin.psmfc.org', name, password) #getPass::getPass('PacFIN User: '), getPass::getPass('Password: ')) 
 				
 				#
